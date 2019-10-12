002a8fd7bb13a11ba3ae2a3cb6458c3420891264
João Pedro <joaopedro453@hotmail.com>
2019-10-11 21:02:41 -0300

    plataformas

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ var config = {
 		update: update
 	}
 };
-
+var platforms;
 var game = new Phaser.Game(config);
 
 function preload() {
@@ -31,8 +31,17 @@ function preload() {
 	);
 }
 
+
 function create() {
 	this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update() {}
